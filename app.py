00b8238e73943b359c0083956fc4f0a95cc33191
bps1372 <asistenprakombps1372.maganghub@gmail.com>
2026-02-02 08:50:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ st.markdown("<div class='subtitle'>Aplikasi data scrapping berita pada situs ber
 
 with st.sidebar:
     st.header("⚙️ Pengaturan Parameter")
-    keyword = st.text_input("Kata Kunci, Contoh: Kota Solok")
+    keyword = st.text_input("Keyword, Contoh: Kota Solok")
     tahun = st.text_input("Tahun, Contoh: 2026")
     max_pages = st.number_input("Jumlah Halaman ", min_value=1, max_value=1000, value=50)
     start_button = st.button("🚀 Mulai Scrapping")
