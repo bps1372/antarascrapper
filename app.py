b45e7472f2c3190a26da689b616087af19eeaf09
bps1372 <asistenprakombps1372.maganghub@gmail.com>
2026-02-02 09:07:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ st.markdown(f"""
 st.markdown("""
 <div class='subtitle'>
 Aplikasi data scrapping berita pada situs berita ANTARA Sumbar (https://sumbar.antaranews.com/) berdasarkan keyword dan tahun. <br> <br>
-Penggunaan data juga sangat bisa digunakan salah satunya dalam rangka pemenuhan Fenomena PDRB Triwulanan.
+Penggunaan data ini juga sangat bisa digunakan salah satunya dalam rangka pemenuhan Fenomena PDRB Triwulanan.
 </div>
 """, unsafe_allow_html=True)
 
